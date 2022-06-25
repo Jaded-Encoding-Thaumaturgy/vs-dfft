fe0ba62aa1c65cefbcbe7acde66de189f98b63a0
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-25 19:46:51 +0200

    Bump up version

--- vsdfft/_metadata.py
+++ vsdfft/_metadata.py
@@ -1,6 +1,6 @@
 """Collection of Discrete/Fast Fourier Transform VapourSynth functions"""
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
